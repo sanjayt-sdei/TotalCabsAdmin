a3adbe9089f0a81b5e3c0034b68881a603761c5f
Nayan Prakash <nayan.prakashos@smartdatainc.net>
2021-01-21 17:42:50 +0530

    first commit

--- server.js
+++ server.js
@@ -1,18 +1,3 @@
-const compression = require('compression');
-//Install express server
-const express = require('express');
-const fallback = require('express-history-api-fallback');
-const app = express();
-
-// compress all responses
-app.use(compression());
-
-// Serve only the static files form the dist directory
-var root = __dirname + '/dist';
-app.use(express.static(root));
-
-app.use(fallback('index.html', { root: root }));
-
 // Start the app by listening on the default Heroku port
 // app.listen(process.env.PORT || 6081, () => {
 //     console.log(`Server running on ${(process.env.PORT || 6081)}`);
@@ -22,3 +7,27 @@ var port = process.env.PORT || 6081;
 app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
+
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+const app = express();
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+extended: false
+}));
+// Point static path to dist
+app.use(express.static(path.join(__dirname, '/dist/')));
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+res.sendFile(path.join(__dirname, '/dist/'));
+});
+/ Get port from environment and store in Express. /
+const port = process.env.PORT || '6081';
+app.set('port', port);
+/ Create HTTP server. /
+const server = http.createServer(app);
+/ Listen on provided port, on all network interfaces. /
+server.listen(port, () =>  console.log(`APP is running on localhost: ${port}`))
