be8d1ce9531c8e3a9e79199421838717a6ed5ee8
Nayan Prakash <nayan.prakashos@smartdatainc.net>
2021-01-21 17:55:32 +0530

    first commit

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
-// Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 6081, () => {
-//     console.log(`Server running on ${(process.env.PORT || 6081)}`);
-// });
-
-// var port = process.env.PORT || 6081;
-// app.listen(port, function() {
-//   console.log('Express server listening on port ' + port);
-// });
-
 const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
 const app = express();
+
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-extended: false
+    extended: false
 }));
+
 // Point static path to dist
 app.use(express.static(path.join(__dirname, '/dist/')));
 
@@ -27,7 +19,7 @@ res.sendFile(path.join(__dirname, '/dist/'));
 });
 
 // Get port from environment and store in Express.
-const port = process.env.PORT || '6081';
+const port = process.env.PORT || 6081;
 app.set('port', port);
 
 // Create HTTP server.
