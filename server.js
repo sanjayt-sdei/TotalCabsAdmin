38161170b453eaeb564f513940bc20aaaf073bfb
Nayan Prakash <nayan.prakashos@smartdatainc.net>
2021-01-21 17:49:56 +0530

    first commit

--- server.js
+++ server.js
@@ -20,14 +20,18 @@ extended: false
 }));
 // Point static path to dist
 app.use(express.static(path.join(__dirname, '/dist/')));
+
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
 res.sendFile(path.join(__dirname, '/dist/'));
 });
-/ Get port from environment and store in Express. /
+
+// Get port from environment and store in Express.
 const port = process.env.PORT || '6081';
 app.set('port', port);
-/ Create HTTP server. /
+
+// Create HTTP server.
 const server = http.createServer(app);
-/ Listen on provided port, on all network interfaces. /
+
+// Listen on provided port, on all network interfaces.
 server.listen(port, () =>  console.log(`APP is running on localhost: ${port}`))
